530b67e6c2220d700b90bfbbfa0f5c72bb87cf81
Alish Rani <94797867+AlishRani@users.noreply.github.com>
2021-12-06 22:53:16 +0530

    Add files via upload

---
+++ sumofnumbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int a;
+   int s=0;
+   printf("enter the value of a\n");
+   scanf("%d",&a);
+   while(a<=10){
+        s=s+0;
+        a=a+1;
+   }
+   printf("sum upto given number%d\n",s);
+   return 0;
+}        
